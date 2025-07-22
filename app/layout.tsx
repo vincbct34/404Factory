81279274a2db85cea9f491d63815ba309afd49b5
Vincent Bichat <vincent.bichat@epitech.eu>
2025-07-22 16:28:52 +0200

    [UPD] Add viewport metadata for improved responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: '404Factory',
   description: 'Created by 404Factory\'s creator',
+  viewport: 'width=device-width, initial-scale=1.0',
 }
 
 export default function RootLayout({
