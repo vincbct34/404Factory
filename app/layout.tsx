ae4bc4df5b50124dc7c8ac39dd7df4b30169509e
Vincent Bichat <vincent.bichat@epitech.eu>
2025-07-22 16:35:02 +0200

    [UPD] Refactor viewport metadata for improved clarity and structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: '404Factory',
-  description: 'Created by 404Factory\'s creator',
-  viewport: 'width=device-width, initial-scale=1.0',
+  description: 'Created by 404Factory\'s creator'
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1.0
 }
 
 export default function RootLayout({
